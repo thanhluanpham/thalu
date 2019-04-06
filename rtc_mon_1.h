f0fd0a105da1bfdc20dfa6ab9adeee9f989bc117
thanhluanpham <thanhluanpham@live.com>
2019-04-06 23:58:41 +0700

    Add files via upload

---
+++ rtc_mon_1.h
@@ -0,0 +1,13 @@
+/*
+ * rtc_mon_1.h
+ *
+ *  Created on: Feb 26, 2019
+ *      Author: Luan_Pham
+ */
+#include "main.h"
+#ifndef RTC_MON_1_H_
+#define RTC_MON_1_H_
+
+
+void RTC_Mon(RTC_TimeTypeDef* sTime);
+#endif /* RTC_MON_1_H_ */
